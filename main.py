0602d865b6938d015a9177bdec0ee61fd0eb0d1d
Harish Kumar T <54526806+Harishkumarharry@users.noreply.github.com>
2022-12-23 22:45:54 +0000

    logic

--- main.py
+++ main.py
@@ -6,3 +6,19 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+print("Welcome to the tip calculator.\n")
+total_Bill = input("What was the total bill? ")
+tip_Percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
+num_People = input("How many people to split the bill? ")
+
+total_Bill_Float = float(total_Bill)
+tip_Percentage_Int = int(tip_Percentage)
+num_People_Int = int(num_People)
+
+tip_Calc = total_Bill_Float * (tip_Percentage_Int / 100)
+
+total_amount = total_Bill_Float + tip_Calc
+
+per_Person = round(total_amount/num_People_Int, 2)
+
+print(f"Each person should pay : ${per_Person}")
